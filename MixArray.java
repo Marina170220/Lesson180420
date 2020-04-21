f6297574abeb0cbe2b1843c3d4fe0167745eaa68
Marina <petrukmn@yandex.ru>
2020-04-21 15:21:11 +0300

    add a random index in a loop

--- MixArray.java
+++ MixArray.java
@@ -9,16 +9,14 @@ public class MixArray {
         int buffer;
         Random random = new Random();
         int[] arrayOfNumbers = {2, 18, 20, 36, 7, 9, 15, 8, 3, 21, 4};
-        int randomIndex = random.nextInt(arrayOfNumbers.length);
+
         for (int i = 0; i < arrayOfNumbers.length; i++) {
-            for (int j = 0; j < arrayOfNumbers.length; j++) {
-                if (i != j) {
-                    buffer = arrayOfNumbers[j];
-                    arrayOfNumbers[j] = arrayOfNumbers[randomIndex];
-                    arrayOfNumbers[randomIndex] = buffer;
-                }
-            }
+            int randomIndex = random.nextInt(arrayOfNumbers.length);
+            buffer = arrayOfNumbers[randomIndex];
+            arrayOfNumbers[randomIndex] = arrayOfNumbers[i];
+            arrayOfNumbers[i] = buffer;
         }
+
         for (int i = 0; i < arrayOfNumbers.length; i++) {
             System.out.print(arrayOfNumbers[i] + " ");
         }
